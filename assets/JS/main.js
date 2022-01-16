f26dea225d6603e6ea33172da0cbd4308b9cef8f
toMilanesi <tomi.milanesi@gmail.com>
2022-01-16 22:05:35 +1100

    added carrito template and styled

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,6 +1,15 @@
 //toma elementos del DOM
+//card section
 const cardContainer = document.getElementById("card-container");
+//carrito section
+const carritoContainer = document.getElementById("carrito-container");
+//carrito footer
+const carritoFooter = document.getElementById("carrito-footer");
+//card template
 const cardTemplate = document.getElementById("card-template").content;
+//carrito template
+const carritoTemplate = document.getElementById("carritoTemplate").content;
+//document fragment
 const fragment = document.createDocumentFragment();
 //carrito de compras
 let carrito = {};
@@ -29,9 +38,9 @@ const pintarCards = data => {
     data.forEach(producto => {
         cardTemplate.getElementById("band").innerHTML = producto.banda;
         cardTemplate.getElementById("titulo").innerHTML = producto.disco;
-        cardTemplate.getElementById("img").setAttribute("src", producto.img)
+        cardTemplate.getElementById("img").setAttribute("src", producto.img);
         cardTemplate.getElementById("btn").dataset.id = producto.id;
-        cardTemplate.getElementById("precio").innerHTML = `$${producto.precio}`;
+        cardTemplate.getElementById("precio").innerHTML = producto.precio;
 
         const clone = cardTemplate.cloneNode(true);
         fragment.appendChild(clone);
@@ -43,6 +52,7 @@ const addCarrito = e => {
     if(e.target.classList.contains("btn-outline-primary")){
         setCarrito(e.target.parentElement);
     }
+    e.stopPropagation();
 }
 
 const setCarrito = objeto =>{
@@ -51,6 +61,7 @@ const setCarrito = objeto =>{
         banda: objeto.querySelector("h4").textContent,
         disco: objeto.querySelector("h5").textContent,
         precio: objeto.querySelector("p").textContent,
+        img: objeto.querySelector("img").getAttribute("src"),
         cantidad: 1
     }
     //aumentar cantidad de mismo producto
@@ -58,10 +69,24 @@ const setCarrito = objeto =>{
         producto.cantidad = carrito[producto.id].cantidad + 1;
     }
 
-    //sumar al carrito como copia del objeto solo se;alando el ID
+    //sumar al carrito como copia del objeto solo senalando el ID
 
     carrito[producto.id] = {...producto};
+    console.log(carrito)
+    pintarCarrito();
+}
 
-    console.log(carrito);
+const pintarCarrito = () =>{
+    carritoContainer.innerHTML = "";
+    Object.values(carrito).forEach(producto =>{
+        carritoTemplate.getElementById("bandaCarrito").innerHTML = producto.banda;
+        carritoTemplate.getElementById("tituloCarrito").innerHTML = producto.disco;
+        carritoTemplate.getElementById("precioCarrito").innerHTML = `$${parseInt(producto.precio) * parseInt(producto.cantidad)}`;
+        carritoTemplate.getElementById("imgCarrito").setAttribute("src",producto.img);
+
+        const clone = carritoTemplate.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    carritoContainer.appendChild(fragment);
 }
 
